3e7fd6e45d1251d43948b9d4edf0c2367b27da4b
Yang Si Jun <aersijun@gmail.com>
2024-03-09 17:03:57 +0800

    carousel done

--- app/src/main/java/com/example/javaandroidapp/Main.java
+++ app/src/main/java/com/example/javaandroidapp/Main.java
@@ -1,13 +1,73 @@
 package com.example.javaandroidapp;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.*;
 
 public class Main extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        RecyclerView categoryRecyclerView = findViewById(R.id.categoryRecyclerView);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false);
+        categoryRecyclerView.setLayoutManager(layoutManager);
+
+        List<String> categories = new ArrayList<>();
+        categories.add("Popular");
+        categories.add("Electronics");
+        categories.add("Consumables");
+        categories.add("Clothes");
+        categories.add("Bags");
+
+        CategoryAdapter adapter = new CategoryAdapter(categories);
+        categoryRecyclerView.setAdapter(adapter);
+    }
+}
+class CategoryAdapter extends RecyclerView.Adapter<CategoryAdapter.CategoryViewHolder> {
+    private List<String> categories;
+
+    public CategoryAdapter(List<String> categories) {
+        this.categories = categories;
+    }
+
+    @NonNull
+    @Override
+    public CategoryViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_category, parent, false);
+        return new CategoryViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull CategoryViewHolder holder, int position) {
+        holder.bind(categories.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return categories.size();
+    }
+
+    static class CategoryViewHolder extends RecyclerView.ViewHolder {
+        private TextView categoryTextView;
+
+        public CategoryViewHolder(@NonNull View itemView) {
+            super(itemView);
+            categoryTextView = itemView.findViewById(R.id.categoryTextView);
+        }
+
+        public void bind(String category) {
+            // Bind data to the views in the item layout
+            categoryTextView.setText(category);
+        }
     }
 }
