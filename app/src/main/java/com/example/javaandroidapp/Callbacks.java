47ef57e68f69da88ed395d7dd545721bfe4005b2
Yang Si Jun <aersijun@gmail.com>
2024-03-18 15:20:23 +0800

    deleted duplciate files

--- app/src/main/java/com/example/javaandroidapp/Callbacks.java
+++
@@ -1,9 +0,0 @@
-package com.example.javaandroidapp;
-
-import java.util.List;
-
-public interface Callbacks {
-    void onResult(boolean isSuccess);
-    void getResult(String result);
-    void getList(List<Listing> listings);
-}
