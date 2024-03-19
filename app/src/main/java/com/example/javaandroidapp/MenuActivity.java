17930f1c899d2157f9c5f4bf2b2b52f19550bb28
ElliotMonde <elliotphua@gmail.com>
2024-03-19 09:59:43 +0800

    init profile/menu activity

---
+++ app/src/main/java/com/example/javaandroidapp/MenuActivity.java
@@ -0,0 +1,13 @@
+package com.example.javaandroidapp;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MenuActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.menu_profile_page);
+    }
+}
