1ba5454cd2239ee4a42eb29e889e5862edb7f4f7
youxiang <ryzeros@gmail.com>
2024-03-25 15:02:40 +0800

    fucked up nvr pull before doing stuff

--- app/src/main/java/com/example/javaandroidapp/activities/MenuActivity.java
+++ app/src/main/java/com/example/javaandroidapp/activities/MenuActivity.java
@@ -11,24 +11,48 @@ import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
 
+import com.bumptech.glide.Glide;
 import com.example.javaandroidapp.R;
+import com.example.javaandroidapp.adapters.CallbackAdapter;
+import com.example.javaandroidapp.objects.User;
+import com.example.javaandroidapp.utils.Users;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.ArrayList;
 
 public class MenuActivity extends AppCompatActivity {
-
-
+    private User user;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.menu_profile_page);
 
+
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        FirebaseAuth mAuth = FirebaseAuth.getInstance();
+        FirebaseUser fbUser = mAuth.getCurrentUser();
         // create new UserProfile instance
-        UserProfile user = new UserProfile();
 
         ImageView profileImageView = findViewById(R.id.profileImageView);
         TextView getUsernameTextView = findViewById(R.id.getUsernameTextView);
         TextView getUserEmailTextView = findViewById(R.id.getUserEmailTextView);
+        Users.getUser(db, fbUser, new CallbackAdapter() {
+            @Override
+            public void getUser(User user_new) {
+                user = user_new;
+                if (user.getProfileImage().equals("")) {
+                    Glide.with(profileImageView).load(R.drawable.profile_pic).into(profileImageView);
+                } else {
+                    Glide.with(profileImageView).load(user.getProfileImage()).into(profileImageView);
+                }
+                getUsernameTextView.setText(user.getName());
+                getUserEmailTextView.setText(user.getUserRef().getEmail().toString());
+            }
+        });
+
+
         ImageButton backBtn = findViewById(R.id.backBtn);
 
         // set back button
@@ -41,15 +65,11 @@ public class MenuActivity extends AppCompatActivity {
         });
 
         // set profile image to display
-        profileImageView.setImageResource(user.getProfileImage());
 
         // set edit info button
         TextView editInfoBtn = findViewById(R.id.editInfoBtn);
 
         // set name and email details
-        getUsernameTextView.setText(user.getUsername());
-        getUserEmailTextView.setText(user.getEmailAddress());
-
         // set add/view listing buttons
         CardView addListing = findViewById(R.id.addListingCard);
         CardView viewListing = findViewById(R.id.viewListingCard);
