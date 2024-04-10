3fb309a1a29b1451dcc3df0d65d5f3295a28904a
Yang Si Jun <aersijun@gmail.com>
2024-04-10 17:34:22 +0800

    edited small stuff and commented failed mass notification

---
+++ app/src/main/java/com/example/javaandroidapp/activities/MerchantOrderDetails.java
@@ -0,0 +1,17 @@
+package com.example.javaandroidapp.activities;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.javaandroidapp.R;
+
+public class MerchantOrderDetails extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.merchant_order_details);
+    }
+}
