a2da4689c224f59b8abe9582f9cb076832b170fe
ElliotMonde <elliotphua@gmail.com>
2024-04-16 02:33:16 +0800

    resolved conflicts in manageOrderActivity

--- app/src/main/java/com/example/javaandroidapp/activities/ManageOrderActivity.java
+++ app/src/main/java/com/example/javaandroidapp/activities/ManageOrderActivity.java
@@ -1,8 +1,11 @@
 package com.example.javaandroidapp.activities;
 
 import android.content.Intent;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.CompoundButton;
 import android.widget.ImageButton;
@@ -10,6 +13,7 @@ import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.Switch;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.OnBackPressedCallback;
 import androidx.annotation.NonNull;
@@ -17,9 +21,18 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.bumptech.glide.Glide;
 import com.example.javaandroidapp.R;
 import com.example.javaandroidapp.modals.Listing;
+import com.example.javaandroidapp.utils.OauthToken;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.material.card.MaterialCardView;
@@ -28,16 +41,33 @@ import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QuerySnapshot;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.w3c.dom.Document;
+
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 public class ManageOrderActivity extends AppCompatActivity {
     public boolean minFulfilled;
+    String SECRET_KEY;
+    ApplicationInfo applicationInfo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.manage_order);
+        try {
+            applicationInfo = getPackageManager().getApplicationInfo(getPackageName(), PackageManager.GET_META_DATA);
+        }
+        catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+        if (applicationInfo != null){
+            SECRET_KEY = applicationInfo.metaData.getString("secretKey");
+        }
 
         Listing listing = (Listing) getIntent().getSerializableExtra("listing");
 
@@ -186,17 +216,118 @@ public class ManageOrderActivity extends AppCompatActivity {
                     @Override
                     public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                         if (isChecked) {
+                            //TODO - once finalised I need to get all ids and notify them
                             buttonView.setClickable(false);
                             docRef.update("deliveryStatus", "Finalised");
                             listing.setDeliveryStatus("Finalised");
-                            db.collection("orders").whereEqualTo("listingId", listing.getUid()).get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                            docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>(){
+                                // I get my listing reference
                                 @Override
-                                public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                                public void onComplete(@NonNull Task<DocumentSnapshot> task) {
                                     if (task.isSuccessful()) {
-                                        QuerySnapshot querySnapshot = task.getResult();
-                                        if (!querySnapshot.isEmpty()) {
-                                            for (DocumentSnapshot doc : querySnapshot) {
-                                                db.collection("orders").document(doc.getId()).update("delivery", "Finalised");
+                                        DocumentSnapshot listingSnapshot = task.getResult();
+                                        if (listingSnapshot != null) {
+                                            // I get the array of orderReference
+                                            ArrayList<DocumentReference> ordersList = (ArrayList<DocumentReference>) listingSnapshot.getData().get("orders");
+                                            Log.d("ordersList", "ordersList" + ordersList);
+                                            // Check if the ordersList is not null and not empty before looping through it
+                                            if (ordersList != null && !ordersList.isEmpty()) {
+                                                // iterate through the order object
+                                                for (DocumentReference orderRef : ordersList) {
+                                                    orderRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                                                        @Override
+                                                        public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                                                            Log.d("orderRef", "orderRef" + orderRef.getId());
+                                                            if (task.isSuccessful()) {
+                                                                DocumentSnapshot orderSnapshot = task.getResult();
+                                                                if (orderSnapshot != null) {
+                                                                    //use order.clientSecret to capture amount
+                                                                    captureIntent(orderSnapshot.getString("paymentId"));
+                                                                    //get user object for device token for fcm
+                                                                    DocumentReference userRef = (DocumentReference) orderSnapshot.getData().get("user");
+                                                                    Log.d("userRef", "userRef" + userRef.getId());
+                                                                    userRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                                                                        @Override
+                                                                        public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                                                                            if (task.isSuccessful()) {
+                                                                                DocumentSnapshot userSnapshot = task.getResult();
+                                                                                if (userSnapshot != null) {
+                                                                                    String deviceToken = (String) userSnapshot.getData().get("userIdToken");
+                                                                                    Log.d("userIdToken", "userIdToken" +  deviceToken);
+                                                                                    //now we have userIdToken
+                                                                                    //we need to use this to generate notif
+                                                                                    // Get OauthToken
+                                                                                    OauthToken.getAccessToken(getApplicationContext(), new OauthToken.AccessTokenCallback(){
+                                                                                        @Override
+                                                                                        public void onSuccess(String new_accessToken) {
+                                                                                            String nameString = listingSnapshot.get("name").toString();
+                                                                                            String pricePaid = orderSnapshot.get("paidAmount").toString();
+                                                                                            // Handle the access token
+                                                                                            JSONObject messageObject = new JSONObject();
+                                                                                            JSONObject notificationObject = new JSONObject();
+                                                                                            JSONObject payloadObject = new JSONObject();
+
+                                                                                            try {
+                                                                                                // Note: I lost a year of lifespan doing this token stuff
+                                                                                                messageObject.put("token", deviceToken);
+                                                                                                notificationObject.put("body", String.format("Your order %s has been finalised by the seller. " +
+                                                                                                        "S$%s has been deducted from your account. " +
+                                                                                                        "Do check the order detail page to track your delivery", nameString, pricePaid));
+                                                                                                notificationObject.put("title", "Your order has been finalised!");
+                                                                                                messageObject.put("notification", notificationObject);
+
+                                                                                                payloadObject.put("message", messageObject);
+
+                                                                                                // Create the request
+                                                                                                JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.POST,
+                                                                                                        "https://fcm.googleapis.com/v1/projects/bulkifydb/messages:send",
+                                                                                                        payloadObject,
+                                                                                                        new Response.Listener<JSONObject>() {
+                                                                                                            @Override
+                                                                                                            public void onResponse(JSONObject response) {
+                                                                                                                //Add toast to log total amount captured
+                                                                                                            }
+                                                                                                        },
+                                                                                                        new Response.ErrorListener() {
+                                                                                                            @Override
+                                                                                                            public void onErrorResponse(VolleyError error) {
+                                                                                                                Log.e("FCM Error", "Error sending FCM message: " + error.toString());
+                                                                                                            }
+                                                                                                        }) {
+                                                                                                    @Override
+                                                                                                    public Map<String, String> getHeaders() throws AuthFailureError {
+                                                                                                        Map<String, String> headers = new HashMap<>();
+                                                                                                        // Add authorization token to headers
+                                                                                                        headers.put("Authorization", "Bearer " + new_accessToken);
+                                                                                                        // Add other headers if needed
+                                                                                                        headers.put("Content-Type", "application/json");
+                                                                                                        return headers;
+                                                                                                    }
+                                                                                                };
+                                                                                                RequestQueue requestQueue = Volley.newRequestQueue(ManageOrderActivity.this);
+                                                                                                requestQueue.add(jsonObjectRequest);
+
+                                                                                            } catch (JSONException e) {
+                                                                                                e.printStackTrace();
+                                                                                            }
+                                                                                        }
+
+                                                                                        @Override
+                                                                                        public void onError(Exception exception) {
+                                                                                            // Handle the error
+                                                                                            Log.e("AccessToken", "Error: " + exception.getMessage());
+                                                                                        }
+                                                                                    });
+
+                                                                                }
+                                                                            }
+                                                                        }
+                                                                    });
+                                                                }
+                                                            }
+                                                        }
+                                                    });
+                                                }
                                             }
                                         }
                                     }
@@ -288,5 +419,36 @@ public class ManageOrderActivity extends AppCompatActivity {
                 optionsLayout4.setVisibility(View.GONE);
                 optionsLayout0.setClickable(false);
         }
+    }
+    private void captureIntent(String paymentId) {
+        StringRequest stringRequest = new StringRequest(Request.Method.POST,
+                String.format("https://api.stripe.com/v1/payment_intents/%s/capture", paymentId),
+                new Response.Listener<String>() {
+
+                    @Override
+                    public void onResponse(String response) {
+                        Log.d("Success", "Success capture");
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        }) {
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                Map<String, String> header = new HashMap<>();
+                header.put("Authorization", "Bearer " + SECRET_KEY);
+                return header;
+            }
+
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> params = new HashMap<>();
+                return params;
+            }
+        };
+        RequestQueue requestQueue = Volley.newRequestQueue(ManageOrderActivity.this);
+        requestQueue.add(stringRequest);
     }
 }
